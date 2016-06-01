f4a9a94ed524b22b282fd9f2de7e277e39b904ad
kaiseer <kais.ghedamsi@champsandchimps.com>
2016-06-01 16:16:29 +0200

    js

---
+++ dist/js/main.js
@@ -0,0 +1,27 @@
+//GÖR EN ARRAY ISTÄLLET!!!!!!!
+
+function startSkills() {
+	var html = document.querySelector('.html');
+	var css = document.querySelector('.css');
+	var javascript = document.querySelector('.javascript');
+	var framework = document.querySelector('.front_framework');
+	var csharp = document.querySelector('.csharp');
+	var php = document.querySelector('.php');
+	var node = document.querySelector('.node');
+	var back_framework = document.querySelector('.back_framework');
+
+	yPos = window.pageYOffset;
+
+	if(yPos > 900) {
+		html.className += ' launched';
+		css.className += ' launched';
+		javascript.className += ' launched';
+		framework.className += ' launched';
+		csharp.className += ' launched';
+		php.className += ' launched';
+		node.className += ' launched';
+		back_framework.className += ' launched';
+	}
+}
+
+window.addEventListener('scroll', startSkills);

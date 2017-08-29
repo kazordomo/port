cbb339db8842781854f93748fc64f71c30628687
kazordomo <zak.eklund@live.se>
2017-08-29 19:33:46 +0200

    made it work again.

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,27 +1,27 @@
-//GÖR EN ARRAY ISTÄLLET!!!!!!!
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
+});
+
+var abilities = document.getElementsByClassName('ability');
 
 function startSkills() {
-	var html = document.querySelector('.html');
-	var css = document.querySelector('.css');
-	var javascript = document.querySelector('.javascript');
-	var framework = document.querySelector('.front_framework');
-	var csharp = document.querySelector('.csharp');
-	var php = document.querySelector('.php');
-	var node = document.querySelector('.node');
-	var back_framework = document.querySelector('.back_framework');
-
 	yPos = window.pageYOffset;
 
-	if(yPos > 880) {
-		html.className += ' launched';
-		css.className += ' launched';
-		javascript.className += ' launched';
-		framework.className += ' launched';
-		csharp.className += ' launched';
-		php.className += ' launched';
-		node.className += ' launched';
-		back_framework.className += ' launched';
-	}
-}
+	if(yPos > 850) {
 
+        for (var i = 0; i < abilities.length; i++) {
+            abilities[i].className += ' launched';
+        }
+    }
+}
 window.addEventListener('scroll', startSkills);
--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
-var gulp 				= require('gulp');
-var jade 				= require('gulp-jade');
-var stylus 			= require('gulp-stylus');
-var watch 			= require('gulp-watch');
+var gulp = require('gulp');
+var jade = require('gulp-jade');
+var stylus = require('gulp-stylus');
+var watch = require('gulp-watch');
 var browserSync = require('browser-sync');
 
 var sources = {
 	jade: 'app/*.jade',
 	stylus: 'app/styl/style.styl'
-}
+};
 
 gulp.task('jade', function() {
 	return gulp.src(sources.jade)
@@ -30,7 +30,7 @@ gulp.task('watch', function() {
 		server: {
 			baseDir: 'dist/'
 		}
-	})
+	});
 	gulp.watch(sources.jade, ['jade-watch']);
 	gulp.watch(sources.stylus, ['stylus-watch']);
 });
